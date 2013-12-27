41f7ac07a4416855508562bf812020f4f4567dd7
David Culpepper <culpedr@gmail.com>
2013-12-27 17:23:51 -0600

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,30 @@
+source 'https://rubygems.org'
+ruby '2.0.0'
+
+group :development, :test do
+  gem 'rspec-rails'
+end
+
+group :test do
+  gem 'selenium-webdriver'
+  gem 'capybara'
+end
+
+gem 'rails', '4.0.0'
+gem 'pg'
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'therubyracer', platforms: :ruby
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 1.2'
+
+group :doc do
+  # bundle exec rake doc:rails generates the API under doc/api.
+  gem 'sdoc', require: false
+end
+
+group :production do
+  gem 'rails_12factor'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = 'f20b359a6467e79bbaae354f84997a6db2bb5328097339b6f0abc26cebf64cff82412b478810f95ed2f3017f621c5225b70b5ca1b0cf87657ee3a8b0f68d6062'
